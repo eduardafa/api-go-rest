e329fc3aef5e226bea67552f8ffaecbdc1a8d775
Eduarda França <eduardafandrade@gmail.com>
2022-12-10 20:04:01 -0330

    feat: middleware that sets content type added

--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"api-go-rest/controllers"
+	"api-go-rest/middleware"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
@@ -9,6 +10,7 @@ import (
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.SetContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/allpersonalities", controllers.GetAllPersonalities).Methods("Get")
 	r.HandleFunc("/api/personality/{id}", controllers.GetPersonalityById).Methods("Get")
