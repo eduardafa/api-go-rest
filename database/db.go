7e8ad8848ec12034dd96332702344e74ab617da0
Eduarda França <eduardafandrade@gmail.com>
2022-12-09 21:50:24 -0330

    feat: connection with postgres linked

--- database/db.go
+++ database/db.go
@@ -1,1 +1,20 @@
 package database
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+)
+
+var (
+	DB  *gorm.DB
+	err error
+)
+
+func ConnectDatabase() {
+	connectionString := "host=localhost user=root password=root dbname=root port=49153 sslmode=disable"
+	DB, err = gorm.Open(postgres.Open(connectionString))
+	if err != nil {
+		log.Panic("Error to connect with database")
+	}
+}
