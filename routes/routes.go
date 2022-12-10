36c11b3bfce084a6c31f4b043f085299de594ab5
Eduarda França <eduardafandrade@gmail.com>
2022-12-10 18:51:36 -0330

    feat: route to create new personality added

--- routes/routes.go
+++ routes/routes.go
@@ -10,7 +10,8 @@ import (
 func HandleRequest() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", controllers.Home)
-	r.HandleFunc("/api/allpersonalities", controllers.AllPersonalities).Methods("Get")
-	r.HandleFunc("/api/personality/{id}", controllers.GetPersonality).Methods("Get")
+	r.HandleFunc("/api/allpersonalities", controllers.GetAllPersonalities).Methods("Get")
+	r.HandleFunc("/api/personality/{id}", controllers.GetPersonalityById).Methods("Get")
+	r.HandleFunc("/api/personality", controllers.CreatePersonality).Methods("Post")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
