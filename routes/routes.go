6fab635e1909fb4042cc54f4ff8d65e3275aecb5
Eduarda França <eduardafandrade@gmail.com>
2022-12-11 01:13:25 -0330

    feat: CORS allowed

--- routes/routes.go
+++ routes/routes.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"api-go-rest/controllers"
 	"api-go-rest/middleware"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
@@ -17,5 +18,5 @@ func HandleRequest() {
 	r.HandleFunc("/api/personality", controllers.CreatePersonality).Methods("Post")
 	r.HandleFunc("/api/personality/{id}", controllers.DeletePersonalityById).Methods("Delete")
 	r.HandleFunc("/api/personality/{id}", controllers.UpdatePersonalityById).Methods("Put")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(":8080", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
